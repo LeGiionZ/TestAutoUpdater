9730851ff1c69b480ee91b7dba411df0e3f062b6
LeGiionZ <legion.shadow97@gmail.com>
2024-05-01 12:44:51 -0600

    Segundo commit N

--- WinFormsApp1/WinFormsApp1/obj/Release/net8.0-windows/WinFormsApp1.AssemblyInfo.cs
+++ WinFormsApp1/WinFormsApp1/obj/Release/net8.0-windows/WinFormsApp1.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinFormsApp1")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b32263d878cedc7ff89a2016f55a23649b47db06")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinFormsApp1")]
